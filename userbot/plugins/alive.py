ea8b12a05716f8f9b5a901c667ba6792cc3fb36c
LEGEND-OS <84609111+LEGEND-OS@users.noreply.github.com>
2021-05-25 10:22:24 -0400

    Add files via upload

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,58 @@
+from userbot import *
+from LEGENDBOT.utils import *
+from userbot.cmdhelp import CmdHelp
+from telethon import events, version
+from telethon.events import NewMessage
+from telethon.tl.custom import Dialog
+from telethon.tl.types import Channel, Chat, User
+
+#-------------------------------------------------------------------------------
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND User"
+
+ludosudo = Config.SUDO_USERS
+
+if ludosudo:
+    sudou = "True"
+else:
+    sudou = "False"
+
+aura = bot.uid
+
+mention = f"[{DEFAULTUSER}](tg://user?id={aura})"
+
+
+PM_IMG = "https://telegra.ph/file/d88acbd81510ccf8f4428.mp4"
+pm_caption ="**LEGENDBOT Is Online**\n\n"
+
+pm_caption += f"**┏━━━━━━━━━━━━━┓**\n"
+pm_caption += f"**┣★ Master : {mention}**\n"
+pm_caption += f"**┣★ Telethon : `{version.__version__}`**\n"
+pm_caption += f"**┣★ LEGENDBOT : {LEGENDversion}**\n"
+pm_caption += f"**┣★ Sudo       : `{sudou}`**\n"
+pm_caption += f"**┣★ Channel   : [Join Here](https://t.me/LEGEND_Userbot)**\n"
+pm_caption += f"**┣★ Creater    : [LEGEND Here](https://t.me/David99q)**\n"
+pm_caption += f"**┗━━━━━━━━━━━━━┛**\n"
+
+pm_caption += "    [✨REPO✨](https://github.com/LEGEND-OS/LEGENDBOT) 🔹 [📜License📜](https://github.com/LEGEND-OS/LEGENDBOT/blob/master/LICENSE)"
+
+
+@bot.on(admin_cmd(outgoing=True, pattern="alive$"))
+@bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
+async def amireallyalive(alive):
+    if alive.fwd_from:
+        return
+    await alive.get_chat()
+    await alive.delete()
+    """ For .alive command, check if the bot is running.  """
+    await borg.send_file(alive.chat_id, PM_IMG, caption=pm_caption)
+    await alive.delete()
+
+
+CmdHelp("alive").add_command(
+  'alive', None, 'Check weather the bot is alive or not'
+).add_command(
+  'LEGEND', None, 'Check weather the bot is alive or not. In your custom Alive Pic and Alive Msg'
+).add_info(
+  'Zinda Hai Kya Bro?'
+).add()
