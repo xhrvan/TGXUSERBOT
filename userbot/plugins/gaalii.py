c53086a593b8b9446ef83f158fbb5bda6138a99b
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-06-25 16:06:50 +0000

    Update gaalii.py

--- userbot/plugins/gaalii.py
+++ userbot/plugins/gaalii.py
@@ -2,7 +2,7 @@ from userbot import bot as LEGENDgalaxy
 from userbot.utils import admin_cmd as choot
 
 x = True
-@LEGENDDavid99q.on(choot(pattern="abuse"))
+@LEGENDgalaxy.on(choot(pattern="abuse"))
 async def yashraid(event):
     while x != False:
        
