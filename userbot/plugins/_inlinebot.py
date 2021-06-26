f67a0c927eb83a8ea2f220c400e901bf752f3261
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-06-26 09:52:50 -0400

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -94,8 +94,8 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             )
         else:
             result = builder.article(
-                "@LEGEND_Userbot",
-                text="""**Hey! This is [LEGENDBOT.](https://t.me/LEGEND_Userbot) \nYou can know more about me from the links given below 👇**""",
+                "TAP HERE",
+                text="""**Hey! This is [LEGENDBOT.](https://t.me/Legend_Mr_Hacker) \nYou can know more about me from the links given below 👇**""",
                 buttons=[
                     [
                         custom.Button.url("🔥 CREATOR 🔥", "https://t.me/Legend_Mr_Hacker"),
@@ -123,7 +123,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         page = int(event.data_match.group(1).decode("UTF-8"))
         veriler = button(page, CMD_HELP)
         await event.edit(
-            f"**Legenday AF** [LEGENDBOT](https://t.me/Legend_Mr_Hacker) __Working...__\n\n**Number of modules installed :** `{len(CMD_HELP)}`\n**page:** {page + 1}/{veriler[0]}",
+            f"**Legenday AF** [LEGENDBOT](https://t.me/Legend_Mr_Hacker) **Working...**\n\n**Number of modules installed :** `{len(CMD_HELP)}`\n**page:** {page + 1}/{veriler[0]}",
             buttons=veriler[1],
             link_preview=False,
         )
