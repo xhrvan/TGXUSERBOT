ac9942971ebf8e481c8cf14c1bd261250ce71d7a
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-06-26 07:55:11 -0400

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -17,7 +17,7 @@ from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 
 from userbot.cmdhelp import CmdHelp
-from LEGENDBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
+from uniborg.utils import admin_cmd, sudo_cmd, edit_or_reply
 
 # ================= CONSTANT =================
 
