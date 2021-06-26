c373d027f6b8355c291a32b2c6c052af33ac20f2
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-06-26 08:14:50 -0400

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -6,20 +6,20 @@ import os
 from telethon import TelegramClient
 from var import Var
 from userbot.Config import Config
-from userbot.utils import load_module 
+from userbot.utils import load_module , start_assistance
 from userbot import LOAD_PLUG, LOGS, LEGENDversion
 from pathlib import Path
 import asyncio
 import telethon.utils
 os.system("pip install -U telethon")
 
+LOAD_USERBOT = os.environ.get("LOAD_USERBOT", True)
+LOAD_ASSISTANT = os.environ.get("LOAD_ASSISTANT", True)    
 
 async def add_bot(bot_token):
     await bot.start(bot_token)
-    bot.me = await bot.get_me() 
+    bot.me = await bot.get_me()
     bot.uid = telethon.utils.get_peer_id(bot.me)
-
-
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
@@ -40,64 +40,6 @@ else:
     else:
         bot.start()
 
-
-import glob
-path = 'userbot/plugins/*.py'
-files = glob.glob(path)
-for name in files:
-    with open(name) as f:
-        path1 = Path(f.name)
-        shortname = path1.stem
-        load_module(shortname.replace(".py", ""))
-
-import userbot._core
-
-print(f"""LEGENDBOT IS ON!!! LEGENDBOT VERSION :- {LEGENDversion}
-CONTACT @LEGEND_MR_HACKER
-OWNER :- @Legend_Mr_Hacker
-CREATOR :- @Legend_Mr_Hacker
-DO .alive OR .ping OR .legend CHECK IF I'M ON!
-IF YOU FACE ANY ISSUE THEN ASK WITH @Legend_Mr_Hacker.""")
-
-import os
-from pathlib import Path
-from sys import argv
-
-import telethon.utils
-from telethon import TelegramClient
-
-from userbot import bot
-from userbot.util import load_module, start_assistant
-from var import Var
-
-LOAD_USERBOT = os.environ.get("LOAD_USERBOT", True)
-LOAD_ASSISTANT = os.environ.get("LOAD_ASSISTANT", True)    
-
-async def add_bot(bot_token):
-    await bot.start(bot_token)
-    bot.me = await bot.get_me()
-    bot.uid = telethon.utils.get_peer_id(bot.me)
-
-
-if len(argv) not in (1, 3, 4):
-    bot.disconnect()
-else:
-    bot.tgbot = None
-    if Var.TG_BOT_USER_NAME_BF_HER is not None:
-        print("Initiating Inline Bot")
-        # ForTheGreatrerGood of beautification
-        bot.tgbot = TelegramClient(
-            "TG_BOT_TOKEN", api_id=Var.APP_ID, api_hash=Var.API_HASH
-        ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
-        print("Initialised Sucessfully")
-        print("Starting Dark AI")
-        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
-        print("Startup Completed")
-    else:
-        bot.start()
-
-
-import glob
 
 if LOAD_USERBOT == True:
     path = "userbot/plugins/*.py"
@@ -124,10 +66,15 @@ if LOAD_ASSISTANT == True:
                 start_assistant(shortname.replace(".py", ""))
             except Exception as er:
                 print(er)
-else:
-    print("Assitant is Not Loading As U Have Disabled")
 
-print("DARK COBRA userbot AND YOUR ASSISTANT is Active Enjoy CONADFDFASDFAD For Updates.")
+import userbot._core
+
+print(f"""LEGENDBOT IS ON!!! LEGENDBOT VERSION :- {LEGENDversion}
+CONTACT @LEGEND_MR_HACKER
+OWNER :- @Legend_Mr_Hacker
+CREATOR :- @Legend_Mr_Hacker
+DO .alive OR .ping OR .legend CHECK IF I'M ON!
+IF YOU FACE ANY ISSUE THEN ASK WITH @Legend_Mr_Hacker.""")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
