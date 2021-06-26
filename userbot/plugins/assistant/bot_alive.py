fd821b75069d4b53ea163d3e338afe946d8e4a38
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-06-26 07:04:46 -0400

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -3,7 +3,7 @@ from telethon import events
 from userbot import ALIVE_NAME, bot
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/ba75256278e8ab0cd521e.jpg"
+PM_IMG = "https://telegra.ph/file/08e2419d8c4acec39fc23.jpg"
 pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
 pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
@@ -13,9 +13,9 @@ pm_caption += "➥ **Current Branch** : `master`\n"
 pm_caption += f"➥ **Version** : `2.0`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
 pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/kaal0408/darkultra/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [Darkultra](https://t.me/Dark_ultra_support_group)\n"
-pm_caption += "[Assistant By DarkCobra](https://t.me/Dark_ultra_support_group)"
+pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/LEGEND-OS/LEGENBOT/blob/master/LICENSE)\n"
+pm_caption += "➥ **Copyright** : By [LEGENDBOT](https://t.me/Legend_Mr_Hacker)\n"
+pm_caption += "[Assistant By LEGEND](https://t.me/Legend_Mr_Hacker)"
 
 # only Owner Can Use it
 @tgbot.on(events.NewMessage(pattern="^/alive", func=lambda e: e.sender_id == bot.uid))
