efc900184cfef5ca19d4efb8129b1973886ea57e
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-06-27 23:53:28 +0530

    Add files via upload

--- userbot/helpers/progress.py
+++ userbot/helpers/progress.py
@@ -1,43 +1,72 @@
-  
-# Copyright (C) 2020 Adek Maulana
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-
-
-import hashlib
+import asyncio
+import datetime
+import importlib
+import inspect
+import logging
 import math
+import os
 import re
+import sys
 import time
+import traceback
+from pathlib import Path
+from time import gmtime, strftime
 
-from .exceptions import CancelProcess
+from telethon import events
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
+
+from hellbot import *
+from hellbot.config import Config
+from hellbot.helpers import *
+from hellbot.utils import *
+
+async def reply_id(event):
+    reply_to_id = None
+    if event.sender_id in Config.SUDO_USERS:
+        reply_to_id = event.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    return reply_to_id
+
+# let's see the progress
+async def progress(
+    current, total, event, start, type_of_ps, file_name=None, is_cancelled=None
+):
+    """Generic progress_callback for uploads and downloads.""" # edit this docstring to your need. If you are kanging it. Lol
+    now = time.time()
+    diff = now - start
+    if is_cancelled is True:
+        raise CancelProcess
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        progress_str = "[{0}{1}] {2}%\n".format(
+            "".join(["▰" for i in range(math.floor(percentage / 10))]),
+            "".join(["▱" for i in range(10 - math.floor(percentage / 10))]),
+            round(percentage, 2),
+        )
+        tmp = progress_str + "{0} of {1}\nETA: {2}".format(
+            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time)
+        )
+        if file_name:
+            await event.edit(
+                "{}\nFile Name: `{}`\n{}".format(type_of_ps, file_name, tmp)
+            )
+        else:
+            await event.edit("{}\n{}".format(type_of_ps, tmp))
 
 
-async def md5(fname: str) -> str:
-    hash_md5 = hashlib.md5()
-    with open(fname, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
-
-
-def humanbytes(size: int) -> str:
-    if size is None or isinstance(size, str):
+# gets output in readable format
+def humanbytes(size):
+    """Input size in bytes,
+    outputs in a human readable format"""
+    if not size:
         return ""
-
+    # 2 ** 10 = 1024
     power = 2 ** 10
     raised_to_pow = 0
     dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
@@ -47,19 +76,7 @@ def humanbytes(size: int) -> str:
     return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
 
 
-def time_formatter(seconds: int) -> str:
-    minutes, seconds = divmod(seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-    days, hours = divmod(hours, 24)
-    tmp = (
-        ((str(days) + " day(s), ") if days else "")
-        + ((str(hours) + " hour(s), ") if hours else "")
-        + ((str(minutes) + " minute(s), ") if minutes else "")
-        + ((str(seconds) + " second(s), ") if seconds else "")
-    )
-    return tmp[:-2]
-
-
+# ok! But Wtf
 def human_to_bytes(size: str) -> int:
     units = {
         "M": 2 ** 20,
@@ -77,42 +94,22 @@ def human_to_bytes(size: str) -> int:
     return int(float(number) * units[unit])
 
 
-async def progress(
-    current, total, gdrive, start, prog_type, file_name=None, is_cancelled=False
-):
-    now = time.time()
-    diff = now - start
-    if is_cancelled is True:
-        raise CancelProcess
+# Inputs time in milliseconds
+# to get beautified time
+# basically a time string
+def time_formatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+        ((str(days) + " day(s), ") if days else "")
+        + ((str(hours) + " hour(s), ") if hours else "")
+        + ((str(minutes) + " minute(s), ") if minutes else "")
+        + ((str(seconds) + " second(s), ") if seconds else "")
+        + ((str(milliseconds) + " millisecond(s), ") if milliseconds else "")
+    )
+    return tmp[:-2]
 
-    if round(diff % 10.00) == 0 or current == total:
-        percentage = current * 100 / total
-        speed = current / diff
-        elapsed_time = round(diff)
-        eta = round((total - current) / speed)
-        if "upload" in prog_type.lower():
-            status = "Uploading"
-        elif "download" in prog_type.lower():
-            status = "Downloading"
-        else:
-            status = "Unknown"
-        progress_str = "`{0}` | [{1}{2}] `{3}%`".format(
-            status,
-            "".join(["▰" for i in range(math.floor(percentage / 10))]),
-            "".join(["▱" for i in range(10 - math.floor(percentage / 10))]),
-            round(percentage, 2),
-        )
-        tmp = (
-            f"{progress_str}\n"
-            f"`{humanbytes(current)} of {humanbytes(total)}"
-            f" @ {humanbytes(speed)}`\n"
-            f"**ETA :**` {time_formatter(eta)}`\n"
-            f"**Duration :** `{time_formatter(elapsed_time)}`"
-        )
-        if file_name:
-            await gdrive.edit(
-                f"**{prog_type}**\n\n"
-                f"**File Name : **`{file_name}`**\nStatus**\n{tmp}"
-            )
-        else:
-            await gdrive.edit(f"**{prog_type}**\n\n" f"**Status**\n{tmp}")
+
+# hellbot
