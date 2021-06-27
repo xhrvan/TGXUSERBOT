46a2a394aa604195ad67d7d7b7b2232b9b2685d8
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-06-27 09:10:48 -0400

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,20 +27,19 @@ if len(argv) not in (1, 3, 4):
 else:
     bot.tgbot = None
     if Var.TG_BOT_USER_NAME_BF_HER is not None:
-         print("Initiating Inline Bot")
+        print("Initiating Inline Bot")
         # ForTheGreatrerGood of beautification
         bot.tgbot = TelegramClient(
             "TG_BOT_TOKEN",
             api_id=Var.APP_ID,
             api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
-            LOGS.info("Checking Telegram Bot Username...")
-            bot.tgbot = TelegramClient(
-        ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
         print("Initialisation finished with no errors")
+        print("Starting Userbot")
+        bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
+        print("Startup Completed")
     else:
         bot.start()
-
 import glob
 path = 'userbot/plugins/*.py'
 files = glob.glob(path)
