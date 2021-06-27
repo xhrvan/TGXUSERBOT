f996a24c90bd2ec2f59be676264e5c0bf8806d65
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-06-27 19:37:12 +0000

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -28,6 +28,8 @@ from userbot.cmdhelp import *
 from LEGENDBOT.utils import *
 from userbot.Config import Config
 
+
+mention = f"[{DEFAULTUSER}](tg://user?id={aura})"
 LEGEND_row = Config.BUTTONS_IN_HELP
 LEGEND_emoji = Config.EMOJI_IN_HELP
 # thats how a lazy guy imports
@@ -80,7 +82,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             veriler = button(0, sorted(CMD_HELP))
             result = await builder.article(
                 f"Hey! Only use .help please",
-                text=f"**Running LEGENDBOT**\n\n__Number of plugins installed__ :`{len(CMD_HELP)}`\n**page:** 1/{veriler[0]}",
+                text=f"**『🔱🇱 🇪 🇬 🇪 🇳 🇩 🔱』➙𖤍 {mention}࿐**\n\n__Number of plugins installed__ :`{len(CMD_HELP)}`\n __Commands__ : `{len(apn)}`\n**page:** 1/{veriler[0]}",
                 buttons=veriler[1],
                 link_preview=False,
             )
