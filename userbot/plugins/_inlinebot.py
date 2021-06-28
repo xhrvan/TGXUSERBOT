432dfdb5c23454bdb35f77d18d47b293f1d04fe2
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-06-28 00:53:38 +0000

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -18,10 +18,15 @@
 from math import ceil
 from re import compile
 import asyncio
+import html
+import os
+import re
+import sys
 
 from telethon.events import InlineQuery, callbackquery
 from telethon.sync import custom
-from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.functions.channels import GetParticipantRequest
+from telethon.tl.function.channels import JoinChannelRequest
 
 from userbot import *
 from userbot.cmdhelp import *
@@ -78,9 +83,13 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         if event.query.user_id == bot.uid and query == "@LEGEND_Userbot":
             rev_text = query[::-1]
             veriler = button(0, sorted(CMD_HELP))
+            apn = []
+            for x in CMD_LIST.values():
+                for y in x:
+                    apn.append(y)
             result = await builder.article(
                 f"Hey! Only use .help please", 
-                text=f"**Running LEGENDBOT**\n\n__Number of plugins installed__ :`{len(CMD_HELP)}`\n**page:** 1/{veriler[0]}",
+                text=f"**『🔱🇱 🇪 🇬 🇪 🇳 🇩 🔱』{ALIVE_NAME}**\n\n__Number of plugins installed__ :`{len(CMD_HELP)}` \n__Commands__ : `{len(apn)}`\n**page:** 1/{veriler[0]}",
                 buttons=veriler[1],
                 link_preview=False,
             )
