191caedb20d1135fad994e657fa630dc75af9fac
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-02 17:35:53 +0000

    Update github.py

--- userbot/plugins/github.py
+++ userbot/plugins/github.py
@@ -3,44 +3,56 @@ Syntax: .github USERNAME"""
 import requests
 
 from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
-
-
-
+from userbot.cmdhelp import CmdHelp
+from userbot import *
 @bot.on(admin_cmd(pattern="github (.*)", outgoing=True))
 @bot.on(sudo_cmd(pattern="github (.*)", allow_sudo=True))
 async def _(event):
-    if event.fwd_from:
-        return
-    input_str = event.pattern_match.group(1)
-    url = "https://api.github.com/users/{}".format(input_str)
-    r = requests.get(url)
-    if r.status_code != 404:
-        b = r.json()
-        avatar_url = b["avatar_url"]
-        html_url = b["html_url"]
-        gh_type = b["type"]
-        name = b["name"]
-        company = b["company"]
-        blog = b["blog"]
-        location = b["location"]
-        bio = b["bio"]
-        created_at = b["created_at"]
-        await borg.send_file(
-            event.chat_id,
-            caption="""Name: [{}]({})
-Type: {}
-Company: {}
-Blog: {}
-Location: {}
-Bio: {}
-Profile Created: {}""".format(
-                name, html_url, gh_type, company, blog, location, bio, created_at
-            ),
-            file=avatar_url,
-            force_document=False,
-            allow_cache=False,
-            reply_to=event,
-        )
-        await event.delete()
-    else:
-        await edit_or_reply(event, "`{}`: {}".format(input_str, r.text))
+async def gitsearch(event):
+    xx = await eor(event, get_string("com_2"))
+    try:
+        usrname = event.pattern_match.group(1)
+    except BaseException:
+        return await xx.edit("`Search for whom? Give me a user name!!`")
+    url = f"https://api.github.com/users/{usrname}"
+    ult = requests.get(url).json()
+    try:
+        uname = ult["login"]
+        uid = ult["id"]
+        upic = ult["avatar_url"]
+        ulink = ult["html_url"]
+        uacc = ult["name"]
+        ucomp = ult["company"]
+        ublog = ult["blog"]
+        ulocation = ult["location"]
+        ubio = ult["bio"]
+        urepos = ult["public_repos"]
+        ufollowers = ult["followers"]
+        ufollowing = ult["following"]
+    except BaseException:
+        return await xx.edit("`No such user found...`")
+    fullusr = f"""
+**[GITHUB]({ulink})**
+**Name** - {uacc}
+**UserName** - {uname}
+**ID** - {uid}
+**Company** - {ucomp}
+**Blog** - {ublog}
+**Location** - {ulocation}
+**Bio** - {ubio}
+**Repos** - {urepos}
+**Followers** - {ufollowers}
+**Following** - {ufollowing}
+"""
+    await xx.delete()
+    await ultroid_bot.send_file(
+        event.chat_id,
+        upic,
+        caption=fullusr,
+        link_preview=False,
+    ) 
+CmdHelp("Github").add_command(
+    'github', 'None', 'Use and See
+    ).add_info(
+    'Its help u to find detail of any github a count'
+).add()
