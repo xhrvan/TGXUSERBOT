468fca24d73fbc1a2dc2c100dca8a6a69454cd44
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-09 21:42:39 +0000

    Create ignore.py

---
+++ userbot/plugins/ignore.py
@@ -0,0 +1,21 @@
+import random
+from . import *
+
+xd = ["Your message has been read and Ignored successfully ┐(´∀｀)┌","Umm My Ears Were Shut 🙉"]
+
+@bot.on(admin_cmd(pattern="ignore$"))
+async def oof(ult):
+  rd = random.choice(xd)
+  await eor(ult, rd)
+ 
+ 
+@bot.on(admin_cmd(pattern="cantignore$") 
+async def oof(ult):
+  await eor(ult, "In Mind: **Im Trying To Ignore But I Cant (ｰ ｰ;)**")
+  
+  
+  CmdHelp("ignore").add_commans(
+  'ignore', None, 'Use and See'
+ ).add_command(
+ 'cantignore', None, 'Use and See'
+ ).add()
