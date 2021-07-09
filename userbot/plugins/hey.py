c774fb2abd7d3c70257168307eac0507bc2599c0
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-09 21:33:04 +0000

    Create hey.py

---
+++ userbot/plugins/hey.py
@@ -0,0 +1,14 @@
+from userbot.cmdhelp import CmdHelp
+@bot.on(admin_cmd(pattern="hey ?(.*)"))
+async def hi(event):
+    giveVar = event.text
+    ult = giveVar[4:5]
+    if not ult:
+        ult = "🌺"
+    await edit_or_reply(
+        event,
+        f"{ult}✨✨{ult}✨{ult}{ult}{ult}\n{ult}✨✨{ult}✨✨{ult}✨\n{ult}{ult}{ult}{ult}✨✨{ult}✨\n{ult}✨✨{ult}✨✨{ult}✨\n{ult}✨✨{ult}✨{ult}{ult}{ult}\n☁☁☁☁☁☁☁☁",
+   )
+CmdHelp("hey").add_command(
+'hey', None, 'Use and See'
+).add()
