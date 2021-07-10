5967747ec49bd127ca6db601f3c0f3abfeb3d22a
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-10 16:01:49 +0000

    Update legend.py

--- userbot/plugins/legend.py
+++ userbot/plugins/legend.py
@@ -3,7 +3,7 @@ import time
 from telethon import version
 from userbot import ALIVE_NAME, StartTime, LEGENDversion
 from LEGENDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
-
+from userbot.cmdhelp import CmdHelp
 
 async def reply_id(event):
     reply_to_id = None
