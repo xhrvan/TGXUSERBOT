bd0a668bd290c555576a3a93c1b686275493271d
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-10 15:50:08 +0000

    Update legend.py

--- userbot/plugins/legend.py
+++ userbot/plugins/legend.py
@@ -54,8 +54,8 @@ def get_readable_time(seconds: int) -> str:
 uptime = get_readable_time((time.time() - StartTime))
 
 
-@bot.on(admin_cmd(outgoing=True, pattern="alive$"))
-@bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
+@bot.on(admin_cmd(outgoing=True, pattern="bot$"))
+@bot.on(sudo_cmd(pattern="bot$", allow_sudo=True))
 async def amireallyalive(alive):
     if alive.fwd_from:
         return
@@ -93,3 +93,5 @@ async def amireallyalive(alive):
             f"**🔱 OWNER :** [LEGEND](t.me/Legend_Mr_Hacker)\n"
             f"**┗━━━━━━━━━━━━━┛**\n",
         )
+
+CmdHelp("botstatus").add_command(
