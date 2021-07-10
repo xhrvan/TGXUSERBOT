58d3f33ffbc4180441a4088a740ec53d249dae58
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-10 02:32:46 +0000

    Update yaadash.py

--- userbot/plugins/yaadash.py
+++ userbot/plugins/yaadash.py
@@ -3,35 +3,35 @@ from . import *
 
 
 @bot.on(admin_cmd(pattern="yaadash"))
-async def yaadash(ult):
-	await ult.edit("***falled while walking***")
+async def _(event):
+	await event.edit("***falled while walking***")
 	await asyncio.sleep(3)
-	await ult.edit("***2 Hours Later***")
+	await event.edit("***2 Hours Later***")
 	await asyncio.sleep(6)
-	await ult.edit("who i am here ?") 
+	await event.edit("who i am here ?") 
 	await asyncio.sleep(1)
-	await ult.edit("Where I Am?")
+	await event.edit("Where I Am?")
 	await asyncio.sleep(1)
-	await ult.edit("What I Am Using?")
+	await event.edit("What I Am Using?")
 	await asyncio.sleep(1)
-	await ult.edit("What Is Phone?")
+	await event.edit("What Is Phone?")
 	await asyncio.sleep(1)
-	await ult.edit("What Is Telegram?")
+	await event.edit("What Is Telegram?")
 	await asyncio.sleep(1)
-	await ult.edit("Why Life Exists?")
+	await event.edit("Why Life Exists?")
 	await asyncio.sleep(1)
-	await ult.edit("What is Language?")
+	await event.edit("What is Language?")
 	await asyncio.sleep(1)
-	await ult.edit("Does Reborn Take Place?")
+	await event.edit("Does Reborn Take Place?")
 	await asyncio.sleep(1)
-	await ult.edit("What Is Thinking?")
+	await event.edit("What Is Thinking?")
 	await asyncio.sleep(1)
-	await ult.edit("Does Superman Exists")
+	await event.edit("Does Superman Exists")
 	await asyncio.sleep(3)
-	await ult.edit("Oof I Guess I Should Die")
+	await event.edit("Oof I Guess I Should Die")
 
 
 
 CmdHelp("yaadash").add_command(
-'yaadash', None, 'Use and see'
+	'yaadash', None, 'Use and see'
 ).add()
