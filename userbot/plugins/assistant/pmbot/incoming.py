ed7f63c28fe682ed2afe57de3443435dff7cd2b2
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-13 12:20:46 +0000

    Create incoming.py

---
+++ userbot/plugins/assistant/pmbot/incoming.py
@@ -0,0 +1,21 @@
+from telethon import events
+
+from . import *
+
+# if incoming
+
+
+@asst.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
+async def on_new_mssg(event):
+    incoming = event.raw_text
+    who = event.sender_id
+    if is_blacklisted(who):
+        return
+    # doesn't reply to that user anymore
+    if incoming.startswith("/"):
+        pass
+    elif who == OWNER_ID:
+        return
+    else:
+        xx = await event.forward_to(OWNER_ID)
+        add_stuff(xx.id, who)
