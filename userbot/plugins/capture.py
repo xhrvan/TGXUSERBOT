fc28ed80e959b690e978a5abbc4a1b256607afb6
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-15 11:44:23 -0400

    Update capture.py

--- userbot/plugins/capture.py
+++ userbot/plugins/capture.py
@@ -24,27 +24,27 @@ async def _(event):
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.binary_location = Config.GOOGLE_CHROME_BIN
-        await hell.edit("Starting Google Chrome BIN")
+        await event.edit("Starting Google Chrome BIN")
         driver = webdriver.Chrome(chrome_options=chrome_options)
         input_str = event.pattern_match.group(1)
         driver.get(input_str)
-        await hell.edit("Calculating Page Dimensions")
+        await event.edit("Calculating Page Dimensions")
         height = driver.execute_script(
             "return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);"
         )
         width = driver.execute_script(
             "return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);"
         )
-        await hell.edit("Painting web-page")
+        await event.edit("Painting web-page")
         driver.set_window_size(width + 100, height + 100)
         im_png = driver.get_screenshot_as_png()
         driver.close()
-        await hell.edit("Stopping Google Chrome BIN")
+        await event.edit("Stopping Google Chrome BIN")
         message_id = event.message.id
         if event.reply_to_msg_id:
             message_id = event.reply_to_msg_id
         with io.BytesIO(im_png) as out_file:
-            out_file.name = "HellBot.ScreenCapture.PNG"
+            out_file.name = "Legendbot.ScreenCapture.PNG"
             await bot.send_file(
                 event.chat_id,
                 out_file,
@@ -56,14 +56,14 @@ async def _(event):
             )
         end = datetime.datetime.now()
         ms = (end - start).seconds
-        await hell.edit(f"Completed screencapture Process in {ms} seconds")
+        await event.edit(f"Completed screencapture Process in {ms} seconds")
     except Exception:
         await eod(hell, traceback.format_exc())
 
 CmdHelp("capture").add_command(
         "screenshot", "<link>", "Gives out the web screenshot of given link via Google Crome Bin in .png format", ".screenshot https://github.com/hellboy-op/hellbot"
 ).add_command(
-	"webshot", "<link>", f"Same as  {hl}screenshot."
+	"webshot", "<link>", f"Same as screenshot."
 ).add_info(
 	"Website Screenshot Maker."
 ).add_warning(
