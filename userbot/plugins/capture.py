25af914f3f6f0997331be7382f3f0f89c8fd670c
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-15 18:58:41 +0000

    Update capture.py

--- userbot/plugins/capture.py
+++ userbot/plugins/capture.py
@@ -61,7 +61,7 @@ async def _(event):
         await eod(hell, traceback.format_exc())
 
 CmdHelp("capture").add_command(
-        "screenshot", "<link>", "Gives out the web screenshot of given link via Google Crome Bin in .png format", ".screenshot https://github.com/hellboy-op/hellbot"
+        "screenshot", "<link>", "Gives out the web screenshot of given link via Google Crome Bin in .png format", ".screenshot https://github.com/LEGEND-OS/LEGENDBOT"
 ).add_command(
 	"webshot", "<link>", f"Same as screenshot."
 ).add_info(
