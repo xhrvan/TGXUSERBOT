6512f74288e3b22a65f6a9e30ce56ad429378b5f
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-18 04:24:30 +0000

    Update pranks.py

--- userbot/plugins/pranks.py
+++ userbot/plugins/pranks.py
@@ -265,7 +265,48 @@ async def LEGENDBOT(LEGENDmemes):
     await LEGENDmemmes.delete()
     await LEGENDmemes.client.send_file(LEGENDmemes.chat_id, LEGEND, reply_to=replied)
 
+    
+    
+    import logging
 
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
+)
+from datetime import datetime
+
+from telethon.tl.functions.channels import EditAdminRequest
+from telethon.tl.types import ChatAdminRights
+
+from userbot.utils import admin_cmd
+
+
+@borg.on(admin_cmd(pattern="prank ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    datetime.now()
+    to_promote_id = None
+    rights = ChatAdminRights(post_messages=True)
+    input_str = event.pattern_match.group(1)
+    reply_msg_id = event.message.id
+    if reply_msg_id:
+        r_mesg = await event.get_reply_message()
+        to_promote_id = r_mesg.sender_id
+    elif input_str:
+        to_promote_id = input_str
+    try:
+        await borg(EditAdminRequest(event.chat_id, to_promote_id, rights, ""))
+    except (Exception) as exc:
+        await event.edit(str(exc))
+    else:
+        await event.edit("Successfully Promoted")
+
+    
+    
+    
+    
+    
+    
 CmdHelp("prank").add_command(
   "phc", "<reply to img> <name> - <comment>", "Changes the given pic to dp and shows a comment in phub with the given name", "<reply to img/stcr> .phc NAME - hello PHUB"
 ).add_command(
@@ -275,5 +316,5 @@ CmdHelp("prank").add_command(
 ).add_command(
   "threats", "<reply to image/sticker>", "Changes the given pic to another pic which shows that pic content is threat to society as that of nuclear bomb"
 ).add_command(
-  "prank", None, "If this module doesn't work then contact admins in @LEGENDSupport"
+  "prank", None, " Its prank promote.If this module doesn't work then contact admins in @Legend_Support"
 ).add()
