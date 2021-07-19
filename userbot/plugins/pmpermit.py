7c5d94d3ef2ed30ded495b01f051b01fcdc18d2b
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-19 17:21:17 +0000

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -127,7 +127,7 @@ async def yes_ucan(event):
     
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"legend_is_here_cant_spam")))
-async def legend_is_better(event):
+async def _(event):
     if event.query.user_id == bot.uid:
         fck_bit = f"Oh! C'mon Master {DEFAULTUSER} Im Try To Get Rid Of This Nigga Pls Dont Touch"
         await event.answer(fck_bit, cache_time=0)
@@ -145,7 +145,7 @@ async def legend_is_better(event):
     )
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"lol_u_think_so")))
-async def legend_is_better(event):
+async def _(event):
     if event.query.user_id == bot.uid:
         fck_bit = f"Oh! C'mon Master {DEFAULTUSER} Im Try To Get Rid Of This Nigga Pls Dont Touch"
         await event.answer(fck_bit, cache_time=0)
@@ -166,7 +166,7 @@ async def legend_is_better(event):
 
 
 @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"he_sucks")))
-async def legend_is_better(event):
+async def _(event):
     if event.query.user_id == bot.uid:
         fck_bit = f"Oh! C'mon Master {DEFAULTUSER} Im Try To Get Rid Of This Nigga Pls Dont Touch"
         await event.answer(fck_bit, cache_time=0)
