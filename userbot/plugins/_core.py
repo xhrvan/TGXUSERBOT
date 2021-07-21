c03d725d562508b30393f30ac33978e204fd0b88
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-21 06:16:21 +0530

    Create _core.py

--- userbot/plugins/_core.py
+++ userbot/plugins/_core.py
@@ -128,6 +128,42 @@ async def load(event):
             f"Sorry, could not load {shortname} because of the following error.\n{str(e)}"
         )
 
+
+
+@bot.on(admin_cmd(pattern=r"cmds"))
+@bot.on(sudo_cmd(pattern=r"cmds", allow_sudo=True))
+async def install(event):
+    if event.fwd_from:
+        return
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    cmd = "ls userbot/plugins"
+    thumb = LEGEND_logo
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    o = stdout.decode()
+    _o = o.split("\n")
+    o = "\n".join(_o)
+    OUTPUT = f"List of Plugins in LEGENDBOT :- \n\n{o}\n\n<><><><><><><><><><><><><><><><><><><><><><><><>\nHELP:- If you want to know the commands for a plugin, do :- \n.plinfo <plugin name> without the < > brackets. \nJoin https://t.me/LEGENDSupport for help."
+    if len(OUTPUT) > 69:
+        with io.BytesIO(str.encode(OUTPUT)) as out_file:
+            out_file.name = "cmd_list.text"
+            LEGEND_file = await bot.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                thumb=thumb,
+                reply_to=reply_to_id,
+            )
+            await edit_or_reply(LEGEND_file, f"Output Too Large. This is the file for the list of plugins in LEGENDBOT.\n\n**BY :-** {DEFAULTUSER}")
+            await event.delete()
+
+
+
 CmdHelp("core").add_command(
   "install", "<reply to a .py file>", "Installs the replied python file if suitable to userbot codes. (TEMPORARILY DISABLED AS HACKERS MAKE YOU INSTALL SOME PLUGINS AND GET YOUR DATA)"
 ).add_command(
