0ea6ff24506fc6c732ada0da1b920b523d9717d6
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-22 08:06:31 +0000

    Update __init__.py

--- userbot/plugins/assistant/__init__.py
+++ userbot/plugins/assistant/__init__.py
@@ -1,1 +1,21 @@
 # LEGENDBOT Assistant
+from . import *
+from telethon import Button, custom
+
+from strings import get_languages, get_string
+from userbot import bot
+from userbot import ALIVE_NAME
+OWNER_NAME = ALIVE_NAME
+OWNER_ID = bot.uid
+
+
+async def setit(event, name, value):
+    try:
+        udB.set(name, value)
+    except BaseException:
+        return await event.edit("`Something Went Wrong`")
+
+
+def get_back_button(name):
+    button = [Button.inline("« Bᴀᴄᴋ", data=f"{name}")]
+    return button
