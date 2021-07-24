5681bb5cd1a4d7007a7a26e7d1c1b01b44984a62
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-24 05:27:48 +0000

    Update awake.py

--- userbot/plugins/awake.py
+++ userbot/plugins/awake.py
@@ -7,6 +7,7 @@ import asyncio
 from telethon import events
 from userbot import ALIVE_NAME, CMD_HELP
 from LEGENDBOT.utils import *
+from userbot import *
 from userbot.cmdhelp import CmdHelp
 from telethon import version, events
 from math import ceil
@@ -19,18 +20,20 @@ import re
 from telethon import events, errors, custom
 import io
 from platform import python_version, uname
+
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 
 
    PM_IMG = "https://telegra.ph/file/805dfd46e7019bb2a0b80.jpg"
-   pm_caption = f"      **🔱ℓєgєи∂ϐοτ IS Awake🔱**\n\n"
+   pm_caption ="      🔱ℓєgєи∂ϐοτ IS Awake🔱\n\n"
    
-   pm_caption += f"**🇮🇳τєℓєτнοи : τєℓєτнοи-1.19.0**\n"
-   pm_caption += f"**🇮🇳ργτнοи   : ρყƭɦσɳ-3.8.5**\n"
-   pm_caption += f"**🇮🇳 I'll Be With You Master Till My Dyno Ends!!☠**\n"
-   pm_caption += f"**🇮🇳 οωиєя   : @Legend_Mr_Hacker**\n"
-   pm_caption += f"**🇮🇳 ϐοѕѕ😊  : {DEFAULTUSER}**\n"
-   pm_caption += f"**🇮🇳 gяουρ   : [gяουρ](https://t.me/Legend_Userbot)**\n\n" 
+   pm_caption += f"🇮🇳τєℓєτнοи : τєℓєτнοи-1.19.0\n"
+   pm_caption += f"🇮🇳ργτнοи   : ρყƭɦσɳ-3.8.5\n"
+   pm_caption += f"🇮🇳 I'll Be With You Master Till My Dyno Ends!!☠\n"
+   pm_caption += f"🇮🇳 οωиєя   : @Legend_Mr_Hacker\n"
+   pm_caption += f"🇮🇳 ϐοѕѕ😊  : {DEFAULTUSER}\n"
+   pm_caption += f"🇮🇳 gяουρ   : [gяουρ](https://t.me/Legend_Userbot)\n\n" 
+
    pm_caption += " [✨REPO✨](https://github.com/LEGEND-OS/LEGENDBOT) 🔹 [📜License📜](https://github.com/LEGEND-OS/LEGENDBOT/blob/master/LICENSE)"
 
 @bot.on(admin_cmd(outgoing=True, pattern="awake$"))
@@ -41,7 +44,7 @@ async def amireallyalive(alive):
    await alive.get_chat()
    await alive.delete()
    """ For .awake command, check if the bot is running. """
-   await borg.send_file(alive.chat_id, LEGEND_IMG, caption=pm_caption)
+   await borg.send_file(alive.chat_id, PM_IMG, caption=pm_caption)
    await alive.delete()
                 
            
