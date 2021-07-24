156eb8cfdee12e3d5829a53e999e48f053ccb7dd
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-24 08:13:12 +0530

    Create awake.py

--- userbot/plugins/awake.py
+++ userbot/plugins/awake.py
@@ -19,17 +19,23 @@ import re
 from telethon import events, errors, custom
 import io
 from platform import python_version, uname
+from userbot import Config
+LEGEND_IMG = Config.AWAKE_PIC
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 
-PM_IMG = "https://telegra.ph/file/baf1bde222c614d6040e9.jpg"
-pm_caption ="**🔱LEGENDBOT IS Awake🔱**\n\n"
-pm_caption += f"**🇮🇳 Telethon : TELETHON-1.19.0**\n"
-pm_caption += f"**🇮🇳 Python : PYTHON-3.8.5**\n"
+if LEGEND_IMG:
+pm_caption =f"**   🔱ℓєgєи∂ϐοτ IS Awake🔱**\n\n"
+
+pm_caption += f"**🇮🇳τєℓєτнοи : τєℓєτнοи-1.19.0**\n"
+pm_caption += f"**🇮🇳ργτнοи   : PYTHON-3.8.5**\n"
 pm_caption += f"**🇮🇳 I'll Be With You Master Till My Dyno Ends!!☠**\n"
-pm_caption += f"**🇮🇳 OWNER` : @Legend_Mr_Hacker**\n"
-pm_caption += f"**🇮🇳 MY BOSS😊: {DEFAULTUSER}**\n"
-pm_caption += f"**🇮🇳 CREATOR : [LEGEND](https://t.me/Legend_Mr_Hacker)**\n" 
+pm_caption += f"**🇮🇳 οωиєя   : @Legend_Mr_Hacker**\n"
+pm_caption += f"**🇮🇳 ϐοѕѕ😊  : {DEFAULTUSER}**\n"
+pm_caption += f"**🇮🇳 gяουρ   : [gяουρ](https://t.me/Legend_Userbot)**\n\n" 
 pm_caption += " [✨REPO✨](https://github.com/LEGEND-OS/LEGENDBOT) 🔹 [📜License📜](https://github.com/LEGEND-OS/LEGENDBOT/blob/master/LICENSE)" 
+
+
+
 @bot.on(admin_cmd(outgoing=True, pattern="awake$"))
 @bot.on(sudo_cmd(pattern="awake$", allow_sudo=True))
 async def amireallyalive(alive):
@@ -37,8 +43,8 @@ async def amireallyalive(alive):
       return
    await alive.get_chat()
    await alive.delete()
-   """ For .alive command, check if the bot is running. """
-   await borg.send_file(alive.chat_id, PM_IMG, caption=pm_caption)
+   """ For .awake command, check if the bot is running. """
+   await borg.send_file(alive.chat_id, LEGEND_IMG, caption=pm_caption)
    await alive.delete()
                 
            
