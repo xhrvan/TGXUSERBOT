5789126130c8d006a8c262f79558fb2e2c7a785a
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-24 08:40:53 +0530

    Create awake.py

--- userbot/plugins/awake.py
+++ userbot/plugins/awake.py
@@ -24,17 +24,15 @@ LEGEND_IMG = Config.AWAKE_PIC
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 
 if LEGEND_IMG:
-pm_caption = f"      **🔱ℓєgєи∂ϐοτ IS Awake🔱**\n\n"
-
-pm_caption += f"**🇮🇳τєℓєτнοи : τєℓєτнοи-1.19.0**\n"
-pm_caption += f"**🇮🇳ργτнοи   : ρყƭɦσɳ-3.8.5**\n"
-pm_caption += f"**🇮🇳 I'll Be With You Master Till My Dyno Ends!!☠**\n"
-pm_caption += f"**🇮🇳 οωиєя   : @Legend_Mr_Hacker**\n"
-pm_caption += f"**🇮🇳 ϐοѕѕ😊  : {DEFAULTUSER}**\n"
-pm_caption += f"**🇮🇳 gяουρ   : [gяουρ](https://t.me/Legend_Userbot)**\n\n" 
-pm_caption += " [✨REPO✨](https://github.com/LEGEND-OS/LEGENDBOT) 🔹 [📜License📜](https://github.com/LEGEND-OS/LEGENDBOT/blob/master/LICENSE)" 
-
-
+   pm_caption = f"      **🔱ℓєgєи∂ϐοτ IS Awake🔱**\n\n"
+   
+   pm_caption += f"**🇮🇳τєℓєτнοи : τєℓєτнοи-1.19.0**\n"
+   pm_caption += f"**🇮🇳ργτнοи   : ρყƭɦσɳ-3.8.5**\n"
+   pm_caption += f"**🇮🇳 I'll Be With You Master Till My Dyno Ends!!☠**\n"
+   pm_caption += f"**🇮🇳 οωиєя   : @Legend_Mr_Hacker**\n"
+   pm_caption += f"**🇮🇳 ϐοѕѕ😊  : {DEFAULTUSER}**\n"
+   pm_caption += f"**🇮🇳 gяουρ   : [gяουρ](https://t.me/Legend_Userbot)**\n\n" 
+   pm_caption += " [✨REPO✨](https://github.com/LEGEND-OS/LEGENDBOT) 🔹 [📜License📜](https://github.com/LEGEND-OS/LEGENDBOT/blob/master/LICENSE)"
 
 @bot.on(admin_cmd(outgoing=True, pattern="awake$"))
 @bot.on(sudo_cmd(pattern="awake$", allow_sudo=True))
