f406b68e945c30473e6a338899945e642d594428
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-24 10:49:39 +0530

    Create Config.py

--- userbot/Config.py
+++ userbot/Config.py
@@ -142,7 +142,7 @@ if ENV:
         MONGO_URI = os.environ.get("MONGO_URI", None)
         #alive
         ALIVE_PIC = os.environ.get("ALIVE_PIC", None)
-        AWAKE_PIC = os.environ.get("AWAKE_PIC", None)
+        AWAKE_PIC = os.environ.get("ALIVE_PIC", None)
         ALIVE_MSG = os.environ.get("ALIVE_MSG", None)
         BOT_PIC = os.environ.get("ALIVE_PIC", None)
         #auto bio
