7b44963a2a34a5b49e029531fc11f658ca37a31f
Krishna Kumar <84609111+LEGEND-OS@users.noreply.github.com>
2021-07-24 10:37:59 +0530

    Create awake.py

--- userbot/plugins/awake.py
+++ userbot/plugins/awake.py
@@ -20,7 +20,7 @@ from telethon import events, errors, custom
 import io
 from platform import python_version, uname
 from userbot.Config import AWAKE_PIC
-LEGEND_IMG = str(AWAKE_PIC) if AWAKE_PIC else https://telegra.ph/file/805dfd46e7019bb2a0b80.jpg
+LEGEND_IMG = str(AWAKE_PIC) if AWAKE_PIC else "https://telegra.ph/file/805dfd46e7019bb2a0b80.jpg"
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 
 if LEGEND_IMG:
