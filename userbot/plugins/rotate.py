b2cf1ca0dca1f2f3f640ebf6bf5abab32055f118
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-08-11 00:08:40 +0000

    Update rotate.py

--- userbot/plugins/rotate.py
+++ userbot/plugins/rotate.py
@@ -1,6 +1,6 @@
 import os
 #MADE BY Legend DONT KANG
-import cv2
+
 from userbot.cmdhelp import CmdHelp
 #MADE BY Legend DONT KANG
 #MADE BY Legend DONT KANG
@@ -34,9 +34,12 @@ async def hmm(event):
     img = Image.open(img)
     rotate_img= img.rotate(int(h))
     rotate_img.save("Legend.png")
-    await event.client.send_file(event.chat_id, "s_h_1_v_a_m_s.png", force_document=False, reply_to=event.reply_to_msg_id)
+    await event.client.send_file(event.chat_id, "Legend.png", force_document=False, reply_to=event.reply_to_msg_id)
     shutil.rmtree(sedpath)
     os.remove("Legend.png")
+    
+    
+    
 CmdHelp("rotate").add_command(
     "rotate", "reply to image", ".rotate reply to any image to turn pic"
 ).add()
