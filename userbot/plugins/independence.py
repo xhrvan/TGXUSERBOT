1e1b0bbb9ccf3d86179ff4d03cccdffc05b12d52
Îτß LêgêñdBõt <84609111+LEGEND-OS@users.noreply.github.com>
2021-08-17 22:41:30 +0000

    Update independence.py

--- userbot/plugins/independence.py
+++ userbot/plugins/independence.py
@@ -32,6 +32,6 @@ async def _(event):
         await event.edit(
             animation_chars[i % 17], link_preview=True
         ) 
-CmdHelp("ιи∂єρєи∂єиϲє").add_command(
+CmdHelp("ιи∂ρи∂иϲє").add_command(
     'independence', None, 'Happy Indpendance Day'
 ).add()
