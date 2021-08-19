aec257d31a223cc28bf5b691fdd619049e668da0
SHRVAN <82793048+xhrvan@users.noreply.github.com>
2021-08-19 10:31:33 +0530

    Update information.py

--- userbot/plugins/information.py
+++ userbot/plugins/information.py
@@ -61,7 +61,7 @@ async def _(event):
 <b>🤖 BOT</b>: {}
 <b>👥 Groups in Common</b>: {}
 
-<b>⚡ <a href='https://t.me/LEGEND_Userbot'>From DataBase of TGXBOT</a> ⚡ </b>
+<b>⚡ <a href='https://t.me/tgxbotz_chat'>From DataBase of TGXBOT</a> ⚡ </b>
 """.format(
         user_id,
         user_id,
