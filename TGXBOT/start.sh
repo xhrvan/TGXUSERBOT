d320ac7ece79a5a8b69a39552090ed2a05637d09
SHRVAN <82793048+xhrvan@users.noreply.github.com>
2021-08-20 15:12:53 +0530

    Update start.sh

--- TGXBOT/start.sh
+++ TGXBOT/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#   Copyright (C) 2021 LEGEND-OS
+#   Copyright (C) 2021 XHRVAN
 #
 #   Poject VΛDΣЯ is free software: 
 #   you can redistribute it and/or modify it under the terms of the GNU General Public License 
