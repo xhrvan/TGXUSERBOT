d270c5a865891e320e04faf8c535b7768c76c3ad
SHRVAN <82793048+xhrvan@users.noreply.github.com>
2021-08-20 15:18:18 +0530

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -3,19 +3,19 @@ from . import *
 from userbot import ALIVE_NAME
 from userbot import bot
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND"
-PM_IMG = "https://telegra.ph/file/2c9be46a59b1497553f06.jpg"
+PM_IMG = "https://telegra.ph/file/1038efe249510a9ef0588.jpg"
 pm_caption = "➥ αssísԵαղԵ ís օղlíղҽ \n\n"
 pm_caption += "➥ ѕγѕτєм ѕτατѕ\n"
 pm_caption += "➥ τєℓєτнοи νєяѕιοи: `1.15.0` \n"
-pm_caption += "➥ ℓєgєϐ∂ϐοτ: `3.8.6` \n"
+pm_caption += "➥ τgxϐοτ: `3.8.6` \n"
 pm_caption += "➥ ∂αταϐαѕє ѕτατυѕ: `Functional`\n"
 pm_caption += "➥ ϲυяяєиτ ϐяαиϲн: `master`\n"
 pm_caption += f"➥ νєяѕιοи : `2.0`\n"
 pm_caption += f"➥ мγ ϐοѕѕ: {DEFAULTUSER} \n"
 pm_caption += "➥ нєяοκυ ∂αταϐαѕє: `AWS - Working Properly`\n\n"
-pm_caption += "➥ **𝓛𝓲𝓬𝓮𝓷𝓼𝓮** : [GNU General Public License v3.0](github.com/LEGEND-OS/LEGENBOT/blob/master/LICENSE)\n"
-pm_caption += "➥ 𝓒𝓸𝓹𝔂𝓻𝓲𝓼𝓱𝓽 : By [lҽցҽղժճօԵ](https://t.me/Tgxbotz_chat)\n"
-pm_caption += "[Assistant By ℓεɠεn∂](https://t.me/Legend_Mr_Hacker)"
+pm_caption += "➥ **ℓιϲєиѕє** : [GNU General Public License v3.0](github.com/XHRVAN/TGXBOT/blob/master/LICENSE)\n"
+pm_caption += "➥ ϲοργяιgнτ : By [τgxϐοτϲнατ](https://t.me/Tgxbotz_chat)\n"
+pm_caption += "[Assistant By τgxϐοτυρ∂ατє](https://t.me/tgxbotz_update)"
 
 # only Owner Can Use it
 @tgbot.on(events.NewMessage(pattern="^/alive", func=lambda e: e.sender_id == bot.uid))
