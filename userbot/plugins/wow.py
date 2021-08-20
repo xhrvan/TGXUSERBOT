71864c6221b3a1a9726558c1dac91a7cdfd82093
SHRVAN <82793048+xhrvan@users.noreply.github.com>
2021-08-20 15:50:00 +0530

    Update wow.py

--- userbot/plugins/wow.py
+++ userbot/plugins/wow.py
@@ -3,7 +3,7 @@
 #keep credits = cool coder😎
 #usage :-.wow
 #plz dont kang it take a lot of time to made😭
-#, plz keep credits of Legend_mr_Hacker ⚡⚡
+#, plz keep credits of xhrvan ⚡⚡
 
 from telethon import events
 import asyncio
