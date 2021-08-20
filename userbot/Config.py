fb59e6bb9e159a6e12f8e80ff49989875f8faf2e
SHRVAN <82793048+xhrvan@users.noreply.github.com>
2021-08-20 15:28:27 +0530

    Update Config.py

--- userbot/Config.py
+++ userbot/Config.py
@@ -151,7 +151,7 @@ if ENV:
         LYDIA_API = os.environ.get("LYDIA_API",None)
         PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", None))
         UPSTREAM_REPO = os.environ.get(
-            "UPSTREAM_REPO", "https://github.com/LEGEND-OS/TGXBOT"
+            "UPSTREAM_REPO", "https://github.com/xhrvan/TGXBOT"
         )
         STRING_SESSION = os.environ.get("STRING_SESSION", None)
         BOT_MODE = os.environ.get("BOT_MODE", "ON")
